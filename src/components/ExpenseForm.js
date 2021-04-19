8d9db8ae75277b39eabf9ef851d9d0b009abe4eb
Your Name <you@example.com>
2021-04-20 05:29:14 +0530

    Added Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -55,19 +55,20 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
             type="text"
             placeholder="Description"
             autoFocus
+            className="text-input"
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
             type="text"
             placeholder="Amount"
+            className="text-input"
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -81,13 +82,15 @@ export default class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            className="textarea"
             value={this.state.note}
             onChange={this.onNoteChange}
           >
           </textarea>
-          <button>Add Expense</button>
+          <div>
+            <button className="button">Save Expense</button>          
+          </div>
         </form>
-      </div>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = (env) => {
 
     return {
         // webpack stuffs
-        entry: './src/app.js',
+        entry: ['babel-polyfill','./src/app.js'],
         output: {
             path: path.join(__dirname, 'public', 'dist'),// cos absolute path is needed
             filename: 'bundle.js'
